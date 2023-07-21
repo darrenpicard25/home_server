ff9c74890159aeea134ee98d507a8ecb62914399
Darren Picard <darrenpicard25@gmail.com>
2023-07-20 22:43:35 -0600

    Setting up docker files and docker compose in prep of deploying and continuous integration

--- user_service/src/main.rs
+++ user_service/src/main.rs
@@ -18,23 +18,27 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:12} - hello handler: {:?}", "HANDLER", params);
     let name = params.name.as_deref().unwrap_or("World");
 
-    format!("Hello, {}!", name)
+    format!("Hello, {name}!")
 }
 
 // e.g. `/hello/Darren`
 async fn handler_hello_2(Path(name): Path<String>) -> impl IntoResponse {
     println!("->> {:12} - hello handler 2: {:?}", "HANDLER", name);
 
-    format!("Hello, {}! From handler 2", name)
+    format!("Hello, {name}! From handler 2")
 }
 
 #[tokio::main]
 async fn main() {
+    const PORT: u16 = 3000;
+    const ADDRESS: Ipv4Addr = Ipv4Addr::UNSPECIFIED;
+
+    println!("Starting Service on {:?}:{}", ADDRESS, PORT);
     let router = Router::new()
         .route("/hello", get(handler_hello))
         .route("/hello/:name", get(handler_hello_2));
 
-    let address = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
+    let address = SocketAddr::from((ADDRESS, PORT));
 
     axum::Server::bind(&address)
         .serve(router.into_make_service())
